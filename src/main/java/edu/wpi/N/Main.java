51218a105b60fb09e33d63942aeaa61004723c64
TianYuFan1 <tianyufan01@gmail.com>
2020-04-17 00:06:01 -0400

    refactored teamname to N

--- src/main/java/edu/wpi/N/Main.java
+++ src/main/java/edu/wpi/N/Main.java
@@ -1,4 +1,4 @@
-package edu.wpi.teamname;
+package edu.wpi.N;
 
 public class Main {
 
--- src/test/java/edu/wpi/N/DefaultTest.java
+++ src/test/java/edu/wpi/N/DefaultTest.java
@@ -1,7 +1,7 @@
 /*-------------------------*/
 /* DO NOT DELETE THIS TEST */
 /*-------------------------*/
-package edu.wpi.teamname;
+package edu.wpi.N;
 
 import org.junit.jupiter.api.Test;
 
