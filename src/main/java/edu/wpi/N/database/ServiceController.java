0293ccdb93b2d0504328ade393c20baa635d4c3b
npelletier2 <npelletier@wpi.edu>
2020-04-18 00:53:26 -0400

    Fixed up some PreparedStatement stuff

--- src/main/java/edu/wpi/N/database/ServiceController.java
+++ src/main/java/edu/wpi/N/database/ServiceController.java
@@ -5,10 +5,12 @@ package edu.wpi.N.database;
 import edu.wpi.N.entities.DbNode;
 import edu.wpi.N.entities.Doctor;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ServiceController {
+    private static Connection con = DbController.getCon();
 
     public static void initService() throws DBException {
         try {
@@ -20,7 +22,7 @@ public class ServiceController {
                 + "field VARCHAR(255) NOT NULL, "
                 + "FOREIGN KEY (location) REFERENCES nodes(nodeID) ON DELETE CASCADE)";
 
-            PreparedStatement state = DbController.getCon().prepareStatement(query);
+            PreparedStatement state = con.prepareStatement(query);
             state.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -43,9 +45,10 @@ public class ServiceController {
     // Chris
     public static boolean deleteDoctor(int docID) throws DBException {
         try {
-            String query = "DELETE FROM doctors WHERE doctorID = '" + docID + "'";
-            PreparedStatement state = DbController.getCon().prepareStatement(query);
-            return true;
+            String query = "DELETE FROM doctors WHERE doctorID = ?";
+            PreparedStatement state = con.prepareStatement(query);
+            state.setInt(1, docID);
+            return state.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             throw new DBException("delete function causing error", e);
@@ -56,10 +59,13 @@ public class ServiceController {
         String key = loc.getNodeID();
         try {
             if (key.contains("DEPT")) {
-                String query = "UPDATE doctors SET name = '" + n + "'," +
-                        "location = '" + key + "', field = '" + f + "' WHERE doctorID = '" + docID + "'";
-                PreparedStatement state = DbController.getCon().prepareStatement(query);
-                return true;
+                String query = "UPDATE doctors SET name = ?, location = ?, field = ? WHERE doctorID = ?";
+                PreparedStatement state = con.prepareStatement(query);
+                state.setString(1, n);
+                state.setString(2, key);
+                state.setString(3, f);
+                state.setInt(4, docID);
+                return state.executeUpdate() > 0;
             }
             System.out.println("nodeID for the location is incorrect");
             return false;
