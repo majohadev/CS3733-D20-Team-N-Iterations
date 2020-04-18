20975b340e9c7ceaf900a5dfe05ba4dc268c43c0
npelletier2 <npelletier@wpi.edu>
2020-04-18 00:45:46 -0400

    Implemented testAddDoctor, changed some stuff to use DbNode

--- src/main/java/edu/wpi/N/database/ServiceController.java
+++ src/main/java/edu/wpi/N/database/ServiceController.java
@@ -10,34 +10,37 @@ import java.sql.SQLException;
 
 public class ServiceController {
 
-  public static void initService() throws DBException {
-    try {
-      String query =
-          "Create Table doctors ("
-              + "doctorID INT NOT NULL PRIMARY KEY,"
-              + "name VARCHAR(255) NOT NULL,"
-              + "location CHAR(10) NOT NULL,"
-              + "field VARCHAR(255) NOT NULL)";
-      PreparedStatement state = DbController.getCon().prepareStatement(query);
-    } catch (SQLException e) {
-      e.printStackTrace();
-      throw new DBException("initService causing error", e);
+    public static void initService() throws DBException {
+        try {
+            String query =
+                "Create Table doctors ("
+                + "doctorID INT NOT NULL PRIMARY KEY, "
+                + "name VARCHAR(255) NOT NULL, "
+                + "location CHAR(10) NOT NULL, "
+                + "field VARCHAR(255) NOT NULL, "
+                + "FOREIGN KEY (location) REFERENCES nodes(nodeID) ON DELETE CASCADE)";
+
+            PreparedStatement state = DbController.getCon().prepareStatement(query);
+            state.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new DBException("initService causing error", e);
+        }
     }
-  }
+    // Nick
+    public static Doctor getDoctor(int id) {
+        return new Doctor(0,"a", new DbNode(), "a");
+    }
+    // Nick
+    public static boolean addDoctor(Doctor d){
+        return addDoctor(d.getId(), d.getName(), d.getLoc(), d.getField());
+    }
+    // Nick
+    public static boolean addDoctor(int id, String name, DbNode location, String field){
+        return false;
+    }
 
-  public static Doctor getDoctor(int id) {
-    return new Doctor(0,"a", new DbNode(), "a");
-  }
-
-  public static boolean addDoctor(Doctor d){
-      return addDoctor(d.getId(), d.getName(), d.getLoc(), d.getField());
-  }
-
-  public static boolean addDoctor(int id, String name, DbNode location, String field){
-      return false;
-  }
-
-
+    // Chris
     public static boolean deleteDoctor(int docID) throws DBException {
         try{
             String query = "DELETE FROM doctors WHERE doctorID = '"+docID+"'";
@@ -48,12 +51,12 @@ public class ServiceController {
             throw new DBException("delete function causing error", e);
         }
     }
-
-    public static boolean modifyDoctor(int docID, String n, String loc, String f) throws DBException {
+    // Chris
+    public static boolean modifyDoctor(int docID, String n, DbNode loc, String f) throws DBException {
         try{
-            if(loc.contains("DEPT")){
+            if(loc.getNodeID().contains("DEPT")){
                 String query = "UPDATE doctors SET name = '"+n+"'," +
-                        "location = '"+loc+"', field = '"+f+"' WHERE doctorID = '"+docID+"'";
+                        "location = '"+loc.getNodeID()+"', field = '"+f+"' WHERE doctorID = '"+docID+"'";
                 PreparedStatement state = DbController.getCon().prepareStatement(query);
                 return true;
             }
--- src/test/java/edu/wpi/N/database/ServiceControllerTest.java
+++ src/test/java/edu/wpi/N/database/ServiceControllerTest.java
@@ -18,37 +18,65 @@ public class ServiceControllerTest {
 
   @Test
   public void testGetDoctor() throws DBException {
-    DbController.addNode("NHALL00104", 1250, 850, 4, "Faulkner", "HALL", "Hall 1", "Hall 1", 'N');
-    Doctor jim = new Doctor(1, "jim", DbController.getNode("NHALL00104"), "good");
+      DbController.addNode("NDEPT00104", 1350, 950, 4, "Faulkner", "DEPT", "Cardiology", "Dept 1", 'N');
+      Doctor wong = new Doctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Software Engineering");
 
-    assertTrue(ServiceController.addDoctor(jim));
+      assertTrue(ServiceController.addDoctor(wong));
+      assertEquals(wong, ServiceController.getDoctor(1));
+      assertTrue(ServiceController.deleteDoctor(1));
 
-    assertEquals(jim, ServiceController.getDoctor(1));
-
-    assertTrue(ServiceController.deleteDoctor(1));
-
-    DbController.deleteNode("NHALL00104");
+      DbController.deleteNode("NDEPT00104");
   }
 
   @Test
-  public void testAddDoctor() {}
+  public void testAddDoctor() throws DBException {
+      DbController.addNode("NDEPT00104", 1350, 950, 4, "Faulkner", "DEPT", "Cardiology", "Dept 1", 'N');
+      Doctor wong = new Doctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Software Engineering");
+
+      assertTrue(ServiceController.addDoctor(wong));
+      assertEquals(wong, ServiceController.getDoctor(1));
+      assertTrue(ServiceController.deleteDoctor(1));
+
+      assertTrue(ServiceController.addDoctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Software Engineering"));
+      assertEquals(wong, ServiceController.getDoctor(1));
+      assertTrue(ServiceController.deleteDoctor(1));
+
+
+      DbController.deleteNode("NDEPT00104");
+  }
 
   @Test
   public void testRemoveDoctor() throws DBException {
-      ServiceController.addDoctor(1, "Wilson Wong", "NDEPT00104", "Neurology");
+      DbController.addNode("NDEPT00104", 1350, 950, 4, "Faulkner", "DEPT", "Cardiology", "Dept 1", 'N');
+
+      ServiceController.addDoctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Neurology");
       ServiceController.deleteDoctor(1);
       assertNull(ServiceController.getDoctor(1));
+
+      DbController.deleteNode("NDEPT00104");
   }
 
   @Test
   public void testModifyDoctorFalse() throws DBException {
-      ServiceController.addDoctor(1, "Wilson Wong", "NDEPT00104", "Software Engineering");
-      assertFalse(ServiceController.modifyDoctor(1, "Wilson Wang", "NHALL00104", "Algorithms"));
+      DbController.addNode("NDEPT00104", 1350, 950, 4, "Faulkner", "DEPT", "Cardiology", "Dept 1", 'N');
+      DbController.addNode("NHALL00104", 1250, 850, 4, "Faulkner", "HALL", "Hall 1", "Hall 1", 'N');
+
+      ServiceController.addDoctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Software Engineering");
+      assertFalse(ServiceController.modifyDoctor(1, "Wilson Wang", DbController.getNode("NHALL00104"), "Algorithms"));
+
+      DbController.deleteNode("NDEPT00104");
+      DbController.deleteNode("NHALL00104");
   }
 
   @Test
   public void testModifyDoctorTrue() throws DBException {
-      ServiceController.addDoctor(1, "Wilson Wong", "NDEPT00104", "Software Engineering");
-      assertFalse(ServiceController.modifyDoctor(1, "Wilson Wang", "NDEPT00204", "Algorithms"));
+      DbController.addNode("NDEPT00104", 1350, 950, 4, "Faulkner", "DEPT", "Cardiology", "Dept 1", 'N');
+      DbController.addNode("NDEPT00204", 1450, 950, 4, "Faulkner", "DEPT", "Neurology", "Dept 2", 'N');
+
+      ServiceController.addDoctor(1, "Wilson Wong", DbController.getNode("NDEPT00104"), "Software Engineering");
+      assertFalse(ServiceController.modifyDoctor(1, "Wilson Wang", DbController.getNode("NDEPT00204"), "Algorithms"));
+
+      DbController.deleteNode("NDEPT00104");
+      DbController.deleteNode("NDEPT00204");
   }
 }
