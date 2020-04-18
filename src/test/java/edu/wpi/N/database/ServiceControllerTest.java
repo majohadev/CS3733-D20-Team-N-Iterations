2fb6c7319837811a9231a0c269be82872c3f9514
npelletier2 <npelletier@wpi.edu>
2020-04-17 23:10:40 -0400

    Added ServiceControllerTest with some test stubs

---
+++ src/test/java/edu/wpi/N/database/ServiceControllerTest.java
@@ -0,0 +1,20 @@
+package edu.wpi.N.database;
+
+import org.junit.jupiter.api.Test;
+
+public class ServiceControllerTest {
+    @Test
+    public void testGetDoctor(){}
+
+    @Test
+    public void testAddDoctor(){}
+
+    @Test
+    public void testRemoveDoctor(){}
+
+    @Test
+    public void testModifyDoctor(){}
+
+    @Test
+    public void testFieldDoctors(){}
+}
