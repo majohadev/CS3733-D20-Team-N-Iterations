71f37ca254c03c07e197cc2fcbd63bff27701a5b
TianYuFan1 <tianyufan01@gmail.com>
2020-04-18 00:22:47 -0400

    initialized home fxml screen through App

--- src/main/java/edu/wpi/N/App.java
+++ src/main/java/edu/wpi/N/App.java
@@ -1,11 +1,18 @@
 package edu.wpi.N;
 
+import edu.wpi.N.views.HomeController;
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App extends Application {
+  private Stage primaryStage;
+  private AnchorPane rootLayout;
 
   @Override
   public void init() {
@@ -13,10 +20,57 @@ public class App extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) {}
+  public void start(Stage primaryStage) throws IOException {
+    this.primaryStage = primaryStage;
+    this.primaryStage.setTitle("Brigham and Women's Hospital Kiosk Application");
+    initRootLayout();
+  }
 
   @Override
   public void stop() {
     log.info("Shutting Down");
   }
+
+  public Stage getPrimaryStage() {
+    return this.primaryStage;
+  }
+
+  public void initRootLayout() throws IOException {
+    try {
+      // Load root layout from fxml file
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(getClass().getResource("views/Home.fxml"));
+      rootLayout = loader.load();
+
+      // Show the scene containing the root layout
+      Scene scene = new Scene(rootLayout);
+      primaryStage.setScene(scene);
+
+      // Give the controller access to the main app
+      HomeController controller = loader.getController();
+      controller.setMainApp(this);
+
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
+//  public void showPersonOverview() {
+//    try {
+//      FXMLLoader loader = new FXMLLoader();
+//      loader.setLocation(getClass().getResource("personOverview.fxml"));
+//      AnchorPane personOverview = (AnchorPane) loader.load();
+//      // AnchorPane personOverview = (AnchorPane) loader.load();
+//
+//      // Set person overview into the center of root layout.
+//      rootLayout.setCenter(personOverview);
+//
+//      // Give the controller access to the main app.
+//      PersonOverviewController controller = loader.getController();
+//      controller.setMainApp(this);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+//  }
