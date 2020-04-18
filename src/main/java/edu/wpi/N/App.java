040d6e58908386276ad4e12fcf3f4823dde52624
TianYuFan1 <tianyufan01@gmail.com>
2020-04-18 14:55:04 -0400

    changed to entirely full screen and cleaned code

--- src/main/java/edu/wpi/N/App.java
+++ src/main/java/edu/wpi/N/App.java
@@ -1,19 +1,17 @@
 package edu.wpi.N;
 
-import edu.wpi.N.views.HomeController;
+import edu.wpi.N.views.Controller;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App extends Application {
-  private Stage primaryStage;
-  private AnchorPane rootLayout;
+  private Stage masterStage;
 
   @Override
   public void init() {
@@ -22,10 +20,11 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    this.primaryStage = primaryStage;
-    this.primaryStage.setTitle("Brigham and Women's Hospital Kiosk Application");
-    initRootLayout();
-    showHome();
+    // Configure the primary Stage
+    this.masterStage = primaryStage;
+    this.masterStage.setTitle("Brigham and Women's Hospital Kiosk Application");
+    switchScene("views/home.fxml");
+    masterStage.setMaximized(true);
   }
 
   @Override
@@ -33,49 +32,16 @@ public class App extends Application {
     log.info("Shutting Down");
   }
 
-  public Stage getPrimaryStage() {
-    return this.primaryStage;
-  }
-
-  public void initRootLayout() throws IOException {
-    try {
-      // Load root layout from fxml file
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(getClass().getResource("views/Home.fxml"));
-      rootLayout = loader.load();
-
-      // Show the scene containing the root layout
-      Scene scene = new Scene(rootLayout);
-      primaryStage.setScene(scene);
-
-      // Give the controller access to the main app
-      HomeController controller = loader.getController();
-      controller.setMainApp(this);
-
-      primaryStage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  public void showHome() {
-    try {
-      FXMLLoader loader = new FXMLLoader();
-      loader.setLocation(getClass().getResource("views/Home.fxml"));
-      AnchorPane pane = loader.load();
-
-      Stage stage = new Stage();
-      stage.setTitle("Home Screen");
-      stage.initModality(Modality.WINDOW_MODAL);
-      stage.initOwner(primaryStage);
-
-      Scene scene = new Scene(pane);
-      stage.setScene(scene);
-
-      HomeController controller = loader.getController();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public void switchScene(String path) throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource(path));
+    Pane pane = loader.load();
+    Scene scene = new Scene(pane);
+    masterStage.setScene(scene);
+    masterStage.setFullScreen(true);
+    masterStage.setFullScreenExitHint("");
+    masterStage.show();
+    Controller controller = loader.getController();
+    controller.setMainApp(this);
   }
 }
