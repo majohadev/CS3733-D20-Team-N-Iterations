daa51550a968aa83c1e468778e0e72066db13bda
npelletier2 <npelletier@wpi.edu>
2020-04-18 00:05:35 -0400

    equals method for Doctor

--- src/main/java/edu/wpi/N/entities/Doctor.java
+++ src/main/java/edu/wpi/N/entities/Doctor.java
@@ -1,41 +1,49 @@
 package edu.wpi.N.entities;
 
 public class Doctor {
-    private String DoctorID;
-    private String name;
-    private DbNode loc;
-    private String field;
+  private String name;
+  private DbNode loc;
+  private String field;
 
+  public Doctor(String name, DbNode loc, String field){
+    this.name = name;
+    this.loc = loc;
+    this.field = field;
+  }
 
-    public String getDoctorID() {
-        return DoctorID;
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public DbNode getLoc() {
+    return loc;
+  }
+
+  public void setLoc(DbNode loc) {
+    this.loc = loc;
+  }
+
+  public String getField() {
+    return field;
+  }
+
+  public void setField(String field) {
+    this.field = field;
+  }
+
+  public boolean equals(Object o){
+    if(!(o instanceof Doctor)){
+      return false;
     }
 
-    public void setDoctorID(String doctorID) {
-        DoctorID = doctorID;
-    }
+    Doctor other = (Doctor)o;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public DbNode getLoc() {
-        return loc;
-    }
-
-    public void setLoc(DbNode loc) {
-        this.loc = loc;
-    }
-
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
+    return this.name.equals(other.name) &&
+            this.loc.equals(other.loc) &&
+            this.field.equals(other.field);
+  }
 }
