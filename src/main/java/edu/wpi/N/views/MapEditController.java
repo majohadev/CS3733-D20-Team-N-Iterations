b74645ba9d3a9c81b7aafba54181c36d1e8c3de9
TianYuFan1 <tianyufan01@gmail.com>
2020-04-19 17:20:27 -0400

    displays selected node in table

--- src/main/java/edu/wpi/N/views/MapEditController.java
+++ src/main/java/edu/wpi/N/views/MapEditController.java
@@ -7,10 +7,7 @@ import edu.wpi.N.database.DbController;
 import edu.wpi.N.entities.DbNode;
 import java.util.LinkedList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -46,6 +43,10 @@ public class MapEditController implements Controller {
   @FXML Button btn_add_cancel;
   @FXML Button btn_add_save;
 
+  @FXML ListView lst_selected;
+  @FXML Button btn_delete_clear;
+  @FXML Button btn_delete;
+
   HashBiMap<Circle, DbNode> masterNodes; // stores the map nodes and their respective database nodes
   LinkedList<DbNode> allFloorNodes; // stores all the nodes on the floor
   LinkedList<DbNode> selectedNodes; // stores all the selected nodes on the map
@@ -62,38 +63,42 @@ public class MapEditController implements Controller {
     masterNodes = HashBiMap.create();
     tempNode = null;
     populateMap();
-    accordionListener();
+    //    accordionListener();
   }
-
-  public void accordionListener() {
-    acc_modify
-        .expandedPaneProperty()
-        .addListener(
-            (observable, oldValue, newValue) -> {
-              if (newValue != null) {
-                if (newValue.equals(pn_nodes)) {
-                  accordionListenerNodes();
-                } else if (newValue.equals(pn_edges)) {
-                  System.out.println("Edges");
-                }
-              }
-            });
-  }
-
-  public void accordionListenerNodes() {
-    acc_nodes
-        .expandedPaneProperty()
-        .addListener(
-            (observable, oldValue, newValue) -> {
-              if (newValue != null) {
-                if (newValue.equals(pn_nodes_delete)) {
-
-                } else if (newValue.equals(pn_nodes_edit)) {
-
-                }
-              }
-            });
-  }
+  //
+  //  public void accordionListener() {
+  //    acc_modify
+  //        .expandedPaneProperty()
+  //        .addListener(
+  //            (observable, oldValue, newValue) -> {
+  //              if (newValue != null) {
+  //                if (newValue.equals(pn_nodes)) {
+  //                  accordionListenerNodes();
+  //                } else if (newValue.equals(pn_edges)) {
+  //                  System.out.println("Edges");
+  //                }
+  //              }
+  //            });
+  //  }
+  //
+  //  public void accordionListenerNodes() {
+  //    acc_nodes
+  //        .expandedPaneProperty()
+  //        .addListener(
+  //            (observable, oldValue, newValue) -> {
+  //              if (newValue != null) {
+  //                if (newValue.equals(pn_nodes_delete)) {
+  //
+  //                } else if (newValue.equals(pn_nodes_edit)) {
+  //
+  //                }
+  //              }
+  //            });
+  //  }
+  //
+  //  @FXML ListView lst_delete;
+  //  @FXML Button btn_delete_clear;
+  //  @FXML Button btn_delete;
 
   public void onBtnNewNodeClicked() {
     txt_add_longName.setDisable(false);
@@ -105,8 +110,8 @@ public class MapEditController implements Controller {
 
     tempNode = new Circle();
     tempNode.setRadius(6);
-    tempNode.setLayoutX(IMAGE_WIDTH / 2);
-    tempNode.setLayoutY(SCREEN_HEIGHT / 2);
+    tempNode.setCenterX(IMAGE_WIDTH / 2);
+    tempNode.setCenterY(SCREEN_HEIGHT / 2);
     tempNode.setFill(Color.BLACK);
     tempNode.setOpacity(0.7);
     tempNode.setOnMouseDragged(event -> this.onDragNode(event, tempNode));
@@ -130,13 +135,27 @@ public class MapEditController implements Controller {
   public void onBtnSaveClicked() throws DBException {
     String longName = txt_add_longName.getText();
     String shortName = txt_add_longName.getText();
-    String type = txt_add_type.getText();
-    if (longName == null || shortName == null || type == null) {
+    String type = txt_add_type.getText().toUpperCase();
+    if (longName.equals("") || shortName.equals("") || type.equals("") || type.length() != 4) {
       return;
     }
-    int x = (int) (tempNode.getCenterX() / HORIZONTAL_SCALE);
-    int y = (int) (tempNode.getCenterY() / VERTICAL_SCALE);
-    DbController.addNode(x, y, 4, "Faulkner", type, longName, shortName);
+    int x = (int) ((float) tempNode.getCenterX() / HORIZONTAL_SCALE);
+    int y = (int) ((float) tempNode.getCenterY() / VERTICAL_SCALE);
+
+    DbNode newNode = DbController.addNode(x, y, 4, "Faulkner", type, longName, shortName);
+    Circle mapNode = makeMapNode(newNode);
+    pn_display.getChildren().remove(tempNode);
+    pn_display.getChildren().add(mapNode);
+    masterNodes.put(mapNode, newNode);
+    txt_add_longName.clear();
+    txt_add_shortName.clear();
+    txt_add_type.clear();
+    txt_add_longName.setDisable(true);
+    txt_add_shortName.setDisable(true);
+    txt_add_type.setDisable(true);
+    btn_add_newNode.setDisable(false);
+    btn_add_save.setDisable(true);
+    btn_add_cancel.setDisable(true);
   }
 
   public void onDragNode(MouseEvent event, Circle tempNode) {
@@ -167,9 +186,16 @@ public class MapEditController implements Controller {
     if (mapNode.getFill() == Color.PURPLE) {
       mapNode.setFill(Color.RED);
       selectedNodes.add(masterNodes.get(mapNode));
+      Label lbl = new Label();
+      lbl.setText(masterNodes.get(mapNode).getLongName());
+      lst_selected.getItems().add(lbl);
+
     } else {
       mapNode.setFill(Color.PURPLE);
       selectedNodes.remove(masterNodes.get(mapNode));
+      lst_selected
+          .getItems()
+          .removeIf(n -> ((Label) n).getText().equals(masterNodes.get(mapNode).getLongName()));
     }
   }
 }
