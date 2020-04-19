037ca9da639125d07c2f5d38e77dbcc9e35867f1
TianYuFan1 <tianyufan01@gmail.com>
2020-04-19 13:16:04 -0400

    added accordion listeners

--- src/main/java/edu/wpi/N/views/MapEditController.java
+++ src/main/java/edu/wpi/N/views/MapEditController.java
@@ -7,6 +7,8 @@ import edu.wpi.N.database.DbController;
 import edu.wpi.N.entities.DbNode;
 import java.util.LinkedList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -25,6 +27,14 @@ public class MapEditController implements Controller {
   final float VERTICAL_SCALE = (MAP_HEIGHT) / IMAGE_HEIGHT;
 
   @FXML Pane pn_display;
+  @FXML Accordion acc_modify;
+  @FXML TitledPane pn_nodes;
+  @FXML TitledPane pn_edges;
+
+  @FXML Accordion acc_nodes;
+  @FXML TitledPane pn_nodes_add;
+  @FXML TitledPane pn_nodes_delete;
+  @FXML TitledPane pn_nodes_edit;
 
   HashBiMap<Circle, DbNode> masterNodes; // stores the map nodes and their respective database nodes
   LinkedList<DbNode> allFloorNodes; // stores all the nodes on the floor
@@ -40,7 +50,41 @@ public class MapEditController implements Controller {
     allFloorNodes = DbController.floorNodes(4, "Faulkner");
     masterNodes = HashBiMap.create();
     populateMap();
+    accordionListener();
   }
+
+  public void accordionListener() {
+    acc_modify
+        .expandedPaneProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              if (!newValue.equals(null)) {
+                if (newValue.equals(pn_nodes)) {
+                  accordionListenerNodes();
+                } else if (newValue.equals(pn_edges)) {
+                  System.out.println("Edges");
+                }
+              }
+            });
+  }
+
+  public void accordionListenerNodes() {
+    acc_nodes.expandedPaneProperty().addListener((observable, oldValue, newValue) -> {
+      if (!newValue.equals(null)) {
+        if (newValue.equals(pn_nodes_add)) {
+
+        }
+        else if (newValue.equals(pn_nodes_delete)) {
+
+        }
+        else if (newValue.equals(pn_nodes_edit)) {
+
+        }
+      }
+    });
+  }
+
+
 
   public void populateMap() {
     for (DbNode node : allFloorNodes) {
@@ -70,6 +114,4 @@ public class MapEditController implements Controller {
       selectedNodes.remove(masterNodes.get(mapNode));
     }
   }
-
-
 }
