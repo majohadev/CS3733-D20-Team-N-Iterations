266f65abd2c43a2adaf9a649349bd479ba128792
kofichtner <kofichtner@wpi.edu>
2020-04-20 12:49:27 -0400

    adds tests to directions and makes small changes

--- src/main/java/edu/wpi/N/algorithms/Directions.java
+++ src/main/java/edu/wpi/N/algorithms/Directions.java
@@ -13,6 +13,7 @@ public class Directions {
   private ArrayList<String> directions;
   private static LinkedList<DbNode> path;
   private static State state;
+  private static final double TURN_THRESHOLD = 30;
 
   enum State {
     STARTING,
@@ -44,6 +45,7 @@ public class Directions {
         angle = getAngle(i);
         stateChange = !getState(i + 1).equals(state);
       }
+      boolean spag = atIntersection(currNode);
       state = getState(i);
       switch (state) {
         case STARTING:
@@ -66,21 +68,23 @@ public class Directions {
         case CONTINUING: // could add if passing an intersection, "continue past " + landmark
           distance += getDistance(currNode, nextNode);
           if (!message.equals("")) {
-            r = !r;
-            if (r) {
-              directions.add(message + "and proceed down the hallway");
+            if (atEndOfHall(nextNode)) {
+              directions.add(message + "and proceed to the end of the hallway");
             } else {
-              directions.add(message + "and continue down the hallway");
+              directions.add(message + "and proceed down the hallway");
             }
             message = "";
-          } else if (stateChange) {
+          } else if (stateChange || atIntersection(currNode)) {
             if (getLandmark(nextNode) == null) {
               message = "Continue to next corridor" + getDistanceString(distance);
-            } else if (getLandmark(nextNode).equals(nextNode)) {
+            } else if (getLandmark(nextNode)
+                .equals(nextNode)) { // need to check that its not a turn, check angle for pulmonary
               message =
                   "Proceed straight towards "
                       + getLandmark(nextNode).getLongName()
                       + getDistanceString(distance);
+            } else if (atEndOfHall(nextNode)) {
+              message = "Continue to the end of the hallway" + getDistanceString(distance);
             } else {
               message =
                   "Continue past "
@@ -146,13 +150,16 @@ public class Directions {
       return STARTING;
     } else if (i == path.size() - 1) {
       return ARRIVING;
-    } else if (Math.abs(getAngle(i) - getAngle(i - 1)) >= 80) {
+    } else if (Math.abs(getAngle(i) - getAngle(i - 1)) >= TURN_THRESHOLD) {
+      System.out.println(Math.abs(getAngle(i) - getAngle(i - 1)));
       return TURNING;
     } else if (path.get(i).getFloor() != path.get(i + 1).getFloor()) {
       return CHANGING_FLOOR;
     } else if (!path.get(i).getBuilding().equals(path.get(i + 1).getBuilding())) {
       return EXITING;
     } else {
+      System.out.println(Math.abs(getAngle(i) - getAngle(i - 1)));
+
       return CONTINUING;
     }
   }
@@ -170,11 +177,11 @@ public class Directions {
     } else if (angleChange < -180) {
       angleChange += 360;
     }
-    if (angleChange > 60) {
+    if (angleChange >= TURN_THRESHOLD) {
       return "right"; // (" + angleChange + ") ";
-    } else if (angleChange <= -60) {
+    } else if (angleChange <= -1 * TURN_THRESHOLD) {
       return "left"; // (" + angleChange + ") ";
-    } else if (angleChange < 10 && angleChange > -10) {
+    } else if (angleChange < TURN_THRESHOLD && angleChange > -TURN_THRESHOLD) {
       return "straight"; // + angleChange;
     } else {
       System.out.println("New ANGLE: " + angleChange);
@@ -252,6 +259,48 @@ public class Directions {
                 + Math.pow(nextNode.getY() - currNode.getY(), 2));
     double conversion = 1;
     return distance * conversion;
+  }
+
+  /**
+   * Determines if a node is at an intersection by looking for adjacent hallway nodes
+   *
+   * @param node
+   * @return true, if intersection, false otherwise
+   */
+  private static boolean atIntersection(DbNode node) throws DBException {
+    int hallNodeCount = 0;
+    if (node.getNodeType().equals("HALL")) {
+      for (DbNode n : DbController.getAdjacent(node.getNodeID())) {
+        if (n.getNodeType().equals("HALL")) {
+          hallNodeCount++;
+        }
+      }
+      if (hallNodeCount >= 3) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Determines if a node is the last node in a hallway
+   *
+   * @param node
+   * @return true, if the node is at the end of the hall, false otherwise
+   */
+  private static boolean atEndOfHall(DbNode node) throws DBException {
+    int hallNodeCount = 0;
+    if (node.getNodeType().equals("HALL")) {
+      for (DbNode n : DbController.getAdjacent(node.getNodeID())) {
+        if (n.getNodeType().equals("HALL")) {
+          hallNodeCount++;
+        }
+      }
+      if (hallNodeCount <= 1) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /**
--- src/test/java/edu/wpi/N/algorithms/DirectionsTest.java
+++ src/test/java/edu/wpi/N/algorithms/DirectionsTest.java
@@ -7,9 +7,14 @@ import edu.wpi.N.entities.Path;
 import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests for textual directions, should be checked by reading through outputted directions and
+ * seeing if they make sense
+ */
 public class DirectionsTest {
   @BeforeAll
   public static void setup() throws SQLException, ClassNotFoundException, DBException {
@@ -23,12 +28,111 @@ public class DirectionsTest {
     CSVParser.parseCSV(inputEdges);
   }
 
+  /**
+   * Directions for path from first restroom floor 4 to Gastroentology Associates Wording could be
+   * improved ("continue to end of hallway") is something to work on (3/5)
+   *
+   * @throws DBException
+   */
   @Test
   public void directionsTester() throws DBException {
-    Path path = Pathfinder.findPath("NHALL00504", "NHALL00604");
+    Path path = Pathfinder.findPath("NREST00104", "NDEPT00704");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      // System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from far left stairwell floor 4 to first elevator (bottom left), (4.5/5)
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester2() throws DBException {
+    Path path = Pathfinder.findPath("NSTAI00104", "NELEV00Y04");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      // System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from hallway intersection node, straight to node in same hallway (3/5)
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester3() throws DBException {
+    Path path = Pathfinder.findPath("NHALL00704", "NHALL01504");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      // System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from Patsafe to hallway node at top curved hallway (5/5)
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester4() throws DBException {
+    Path path = Pathfinder.findPath("NDEPT01304", "NHALL02204");
     ArrayList<String> directions = path.getDirections();
     for (String s : directions) {
       System.out.println(s);
     }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from Lung Research to Department offices (5/5)
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester5() throws DBException {
+    Path path = Pathfinder.findPath("NDEPT01204", "NDEPT00204");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from Restrooms (top) to IS (4/5) Would be nicer with end of hallway
+   * detection
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester6() throws DBException {
+    Path path = Pathfinder.findPath("NREST00204", "NDEPT02104");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
+  }
+
+  /**
+   * Directions for path from Rheumatology Center to Pulmonary Services Pulmonary services not Works
+   * (4.5/5), had to reduce turn threshold to 20 deg
+   *
+   * @throws DBException
+   */
+  @Test
+  public void directionsTester7() throws DBException {
+    Path path = Pathfinder.findPath("NDEPT00904", "NDEPT01104");
+    ArrayList<String> directions = path.getDirections();
+    for (String s : directions) {
+      // System.out.println(s);
+    }
+    Assertions.assertEquals(directions, path.getDirections());
   }
 }
