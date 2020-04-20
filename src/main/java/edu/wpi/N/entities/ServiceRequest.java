a2cfad0535389bdce805fa36a617a0c7598aa623
Nicholas Wood <woodn123@gmail.com>
2020-04-19 20:57:50 -0400

    adds table stuff

--- src/main/java/edu/wpi/N/entities/ServiceRequest.java
+++
@@ -1,3 +0,0 @@
-package edu.wpi.N.entities;
-
-public class ServiceRequest {}
--- src/main/java/edu/wpi/N/views/AdminController.java
+++ src/main/java/edu/wpi/N/views/AdminController.java
@@ -3,11 +3,14 @@ package edu.wpi.N.views;
 import edu.wpi.N.App;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class AdminController implements Initializable {
 
@@ -17,18 +20,18 @@ public class AdminController implements Initializable {
   @FXML Button btn_laundryReq;
   @FXML Button btn_transReq;
 
-  @FXML TableView<MockData> tbMockData;
+  @FXML TableView<MockData> tbMockData = new TableView<MockData>();
 
-  @FXML TableColumn<MockData, String> tcData;
+  // @FXML TableColumn<MockData, String> tcData;
+  TableColumn tcDataCol = new TableColumn();
 
   @Override
   public void initialize(URL location, ResourceBundle resourceBundle) {
-    // tbMockData.setItems(mockData);
+    tbMockData.setItems(mockData);
+
+    tcDataCol.setCellValueFactory(new PropertyValueFactory<MockData, String>("First Name"));
   }
 
-  /*
   private ObservableList<MockData> mockData =
       FXCollections.observableArrayList(new MockData("Hello"), new MockData("Goodbye"));
-
-   */
 }
