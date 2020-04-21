b2d12e2dfab40ac4d5e1ab749575636e1164722a
npelletier2 <npelletier@wpi.edu>
2020-04-20 21:08:19 -0400

    Fixed getTransLang and setup

--- src/test/java/edu/wpi/N/database/EmployeeControllerTest.java
+++ src/test/java/edu/wpi/N/database/EmployeeControllerTest.java
@@ -26,10 +26,10 @@ public class EmployeeControllerTest {
     EmployeeController.addTranslator("Felix Bignoodle", langs);
     felix = new Translator(1, "Felix Bignoodle", langs);
 
-    langs.clear();
-    langs.add("Gnomish");
-    EmployeeController.addTranslator("Fats Rumbuckle", langs);
-    fats = new Translator(2, "Fats Rumbuckle", langs);
+    LinkedList<String> langs2 = new LinkedList<>();
+    langs2.add("Gnomish");
+    EmployeeController.addTranslator("Fats Rumbuckle", langs2);
+    fats = new Translator(2, "Fats Rumbuckle", langs2);
 
     EmployeeController.addLaundry("Snaps McKraken");
     snaps = new Laundry(3, "Snaps McKraken");
@@ -131,7 +131,8 @@ public class EmployeeControllerTest {
   public void testGetTransLang() throws DBException {
     LinkedList<Translator> translators = EmployeeController.getTransLang("Gnomish");
     assertEquals(2, translators.size());
-    assertTrue(translators.get(0).getName().equals(felix.getName()));
+    assertTrue(translators.contains(felix));
+    assertTrue(translators.contains(fats));
     //    assertTrue(
     //        translators.get(0).getName().equals(fats.getName())
     //            || translators.get(1).getName().equals(fats.getName()));
