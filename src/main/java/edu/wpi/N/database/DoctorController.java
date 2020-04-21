98bfe0b8de2ffa69c1f582c077d8879ee1099c6c
Noah-C-S <noahcscs@gmail.com>
2020-04-20 23:04:26 -0400

    made Doctors use their own ID as a primary key rather than their name

--- src/main/java/edu/wpi/N/database/DoctorController.java
+++ src/main/java/edu/wpi/N/database/DoctorController.java
@@ -2,10 +2,7 @@ package edu.wpi.N.database;
 
 import edu.wpi.N.entities.DbNode;
 import edu.wpi.N.entities.Doctor;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Iterator;
 import java.util.LinkedList;
 
@@ -16,14 +13,15 @@ public class DoctorController {
     try {
       String query =
           "CREATE TABLE doctors ("
-              + "name VARCHAR(255) NOT NULL PRIMARY KEY, "
+              + "doctorID INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,"
+              + "name VARCHAR(255) NOT NULL, "
               + "field VARCHAR(255) NOT NULL)";
 
       PreparedStatement state = con.prepareStatement(query);
       state.execute();
       query =
           "CREATE TABLE location ("
-              + "doctor VARCHAR(255) NOT NULL REFERENCES doctors(name) ON DELETE CASCADE, "
+              + "doctor INT NOT NULL REFERENCES doctors(doctorID) ON DELETE CASCADE, "
               + "nodeID char(10) NOT NULL REFERENCES nodes(nodeID) ON DELETE CASCADE,"
               + "priority INT NOT NULL GENERATED ALWAYS AS IDENTITY,"
               + "PRIMARY KEY (doctor, nodeID))";
@@ -40,26 +38,27 @@ public class DoctorController {
   /**
    * Gets the doctor with the specified name (must be exact)
    *
-   * @param name The exact name of the doctor
+   * @param doctorID The exact name of the doctor
    * @return The doctor asked for
    * @throws DBException On error or doctor not found
    */
-  public static Doctor getDoctor(String name) throws DBException {
+  public static Doctor getDoctor(int doctorID) throws DBException {
     try {
       String query = "SELECT nodeID FROM location WHERE doctor = ? ORDER BY priority";
       PreparedStatement state = con.prepareStatement(query);
-      state.setString(1, name);
+      state.setInt(1, doctorID);
       ResultSet rs = state.executeQuery();
       LinkedList<DbNode> offices = new LinkedList<DbNode>();
       while (rs.next()) {
         offices.add(DbController.getNode(rs.getString("nodeID")));
       }
-      query = "SELECT name, field FROM doctors WHERE name = ?";
+      query = "SELECT doctorID, name, field FROM doctors WHERE doctorID = ?";
       state = con.prepareStatement(query);
-      state.setString(1, name);
+      state.setInt(1, doctorID);
       rs = state.executeQuery();
       if (rs.next()) {
-        return new Doctor(rs.getString("name"), rs.getString("field"), offices);
+        return new Doctor(
+            rs.getInt("doctorID"), rs.getString("name"), rs.getString("field"), offices);
       } else {
         throw new DBException("getDoctor: Doctor not found");
       }
@@ -75,27 +74,30 @@ public class DoctorController {
    * @param name The doctor's name
    * @param field The field in which they work
    * @param offices A linked list of all of their office in order of priority
-   * @return True if successful, false otherwise
+   * @return the id of the doctor created
    * @throws DBException
    */
-  public static boolean addDoctor(String name, String field, LinkedList<DbNode> offices)
+  public static int addDoctor(String name, String field, LinkedList<DbNode> offices)
       throws DBException {
     try {
-      String query = "INSERT INTO doctors VALUES (?, ?)";
-      PreparedStatement state = con.prepareStatement(query);
+      String query = "INSERT INTO doctors (name, field) VALUES (?, ?)";
+      PreparedStatement state = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
       state.setString(1, name);
       state.setString(2, field);
-      if (state.executeUpdate() <= 0) return false;
-      if (offices == null) return true;
+      if (state.executeUpdate() <= 0) throw new DBException("Error: Doctor not added!");
+      ResultSet rs = state.getGeneratedKeys();
+      rs.next();
+      int id = rs.getInt("1");
+      if (offices == null) return id;
       Iterator<DbNode> it = offices.iterator();
       while (it.hasNext()) {
         query = "INSERT INTO location (doctor, nodeID) VALUES (?, ?)";
         state = con.prepareStatement(query);
-        state.setString(1, name);
+        state.setInt(1, id);
         state.setString(2, it.next().getNodeID());
         state.executeUpdate();
       }
-      return true;
+      return id;
     } catch (SQLException e) {
       e.printStackTrace();
       throw new DBException("Unknown error: addDoctor", e);
@@ -105,15 +107,15 @@ public class DoctorController {
   /**
    * Deletes a doctor from the database
    *
-   * @param name The doctor's name (must be exact)
+   * @param doctorID The ID Of the doctor to be deleted
    * @return True if successful, false otherwise
    * @throws DBException on error
    */
-  public static boolean deleteDoctor(String name) throws DBException {
+  public static boolean deleteDoctor(int doctorID) throws DBException {
     try {
-      String query = "DELETE FROM doctors WHERE name = ?";
+      String query = "DELETE FROM doctors WHERE doctorID = ?";
       PreparedStatement state = con.prepareStatement(query);
-      state.setString(1, name);
+      state.setInt(1, doctorID);
       return state.executeUpdate() > 0;
     } catch (SQLException e) {
       e.printStackTrace();
@@ -124,15 +126,15 @@ public class DoctorController {
   /**
    * Adds an office to the specified doctor's list of offices
    *
-   * @param doctor The doctor you want to add an office for
+   * @param doctorID The doctor you want to add an office for
    * @param office The node of the doctor's new office
    * @throws DBException On error
    */
-  public static void addOffice(String doctor, DbNode office) throws DBException {
+  public static void addOffice(int doctorID, DbNode office) throws DBException {
     try {
       String query = "INSERT INTO location (doctor, nodeID) VALUES (?, ?)";
       PreparedStatement stmt = con.prepareStatement(query);
-      stmt.setString(1, doctor);
+      stmt.setInt(1, doctorID);
       stmt.setString(2, office.getNodeID());
       stmt.executeUpdate();
     } catch (SQLException e) {
@@ -144,16 +146,16 @@ public class DoctorController {
   /**
    * Removes an office from a doctor's list of offices
    *
-   * @param doctor The doctor from which to remove the office
+   * @param doctorID The doctor from which to remove the office
    * @param office the office to remove from the doctor's list of offices
    * @return True if successful, false otherwise
    * @throws DBException On error
    */
-  public static boolean removeOffice(String doctor, DbNode office) throws DBException {
+  public static boolean removeOffice(int doctorID, DbNode office) throws DBException {
     try {
       String query = ("DELETE FROM location WHERE doctor = ? AND nodeID = ?");
       PreparedStatement stmt = con.prepareStatement(query);
-      stmt.setString(1, doctor);
+      stmt.setInt(1, doctorID);
       stmt.setString(2, office.getNodeID());
       return stmt.executeUpdate() > 0;
     } catch (SQLException e) {
@@ -171,11 +173,11 @@ public class DoctorController {
   public static LinkedList<Doctor> getDoctors() throws DBException {
     try {
       LinkedList<Doctor> docs = new LinkedList<Doctor>();
-      String query = "SELECT name FROM doctors";
+      String query = "SELECT doctorID FROM doctors";
       PreparedStatement stmt = con.prepareStatement(query);
       ResultSet rs = stmt.executeQuery();
       while (rs.next()) {
-        docs.add(getDoctor(rs.getString("name")));
+        docs.add(getDoctor(rs.getInt("doctorID")));
       }
       return docs;
     } catch (SQLException e) {
@@ -195,12 +197,12 @@ public class DoctorController {
   public static LinkedList<Doctor> searchDoctors(String name) throws DBException {
     try {
       LinkedList<Doctor> docs = new LinkedList<Doctor>();
-      String query = "SELECT name FROM doctors WHERE UPPER(name) LIKE ?";
+      String query = "SELECT doctorID FROM doctors WHERE UPPER(name) LIKE ?";
       PreparedStatement stmt = con.prepareStatement(query);
       stmt.setString(1, "%" + name.toUpperCase() + "%");
       ResultSet rs = stmt.executeQuery();
       while (rs.next()) {
-        docs.add(getDoctor(rs.getString("name")));
+        docs.add(getDoctor(rs.getInt("doctorID")));
       }
       return docs;
     } catch (SQLException e) {
