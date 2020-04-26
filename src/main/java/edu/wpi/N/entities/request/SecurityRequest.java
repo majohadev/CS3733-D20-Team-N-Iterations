9ef148ff22663d3ab3278582142bd577be574dd1
Matt Johannesen <matt.d.johannesen@gmail.com>
2020-04-25 22:49:45 -0400

    Initial commit

---
+++ src/main/java/edu/wpi/N/entities/request/SecurityRequest.java
@@ -0,0 +1,30 @@
+package edu.wpi.N.entities.request;
+
+import java.util.GregorianCalendar;
+
+public class SecurityRequest extends Request {
+
+  private Boolean isEmergency;
+
+  public SecurityRequest(
+      int requestID,
+      int emp_assigned,
+      String reqNotes,
+      String compNotes,
+      String nodeID,
+      GregorianCalendar timeRequested,
+      GregorianCalendar timeCompleted,
+      String status, boolean isEmergency) {
+    super(
+        requestID, emp_assigned, reqNotes, compNotes, nodeID, timeRequested, timeCompleted, status);
+    this.isEmergency = isEmergency;
+  }
+
+  public String getAtr1() {
+    return isEmergency.toString();
+  }
+
+  public String getServiceType() {
+    return "Security";
+  }
+}
