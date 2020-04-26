2d15f1c0d22fcc68a10f9c8a3b1f911d5fb37580
mjlaks <46726429+mjlaks@users.noreply.github.com>
2020-04-25 21:53:16 -0400

    added a working template for services and services page

--- src/main/java/edu/wpi/N/Main.java
+++ src/main/java/edu/wpi/N/Main.java
@@ -9,6 +9,12 @@ public class Main {
   public static void main(String[] args)
       throws SQLException, DBException, ClassNotFoundException, FileNotFoundException {
     MapDB.initDB();
+    /*final String DEFAULT_NODES = "csv/UPDATEDTeamNnodes.csv";
+    final String DEFAULT_PATHS = "csv/UPDATEDTeamNedges.csv";
+    final InputStream INPUT_NODES_DEFAULT = Main.class.getResourceAsStream(DEFAULT_NODES);
+    final InputStream INPUT_EDGES_DEFAULT = Main.class.getResourceAsStream(DEFAULT_PATHS);
+    CSVParser.parseCSV(INPUT_NODES_DEFAULT);
+    CSVParser.parseCSV(INPUT_EDGES_DEFAULT);*/
     App.launch(App.class, args);
   }
 }
--- src/main/java/edu/wpi/N/views/TemplateController.java
+++ src/main/java/edu/wpi/N/views/TemplateController.java
@@ -1,7 +1,7 @@
 package edu.wpi.N.views;
 
-import com.jfoenix.controls.JFXAutoCompletePopup;
 import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
 import edu.wpi.N.App;
 import edu.wpi.N.algorithms.FuzzySearchAlgorithm;
 import edu.wpi.N.database.DBException;
@@ -11,21 +11,25 @@ import java.util.LinkedList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.input.KeyEvent;
 
 public class TemplateController implements Controller {
 
   private App mainApp;
 
-  // @FXML JFXTextField txt_enterLocation;
+  // Add FXML Tags Here
   @FXML JFXComboBox<String> cmbo_text;
   @FXML JFXComboBox<String> cmbo_selectLang;
-  @FXML JFXAutoCompletePopup<String> autoCompletePopup = new JFXAutoCompletePopup<>();
+  @FXML JFXTextArea txtf_langNotes;
 
   private ObservableList<String> fuzzySearchTextList =
       // List that fills TextViews
       FXCollections.observableArrayList();
   LinkedList<DbNode> fuzzySearchNodeList = new LinkedList<>();
+  DbNode currentNode = null;
+
+  private String countVal = "";
 
   public TemplateController() throws DBException {}
 
@@ -35,33 +39,22 @@ public class TemplateController implements Controller {
 
   public void initialize() throws DBException {
 
-    // SelectionHandler sets the value of the comboBox
-    autoCompletePopup.setSelectionHandler(
-        event -> {
-          cmbo_text.setValue(event.getObject());
-        });
-    TextField editor = cmbo_text.getEditor();
-    editor
-        .textProperty()
-        .addListener(
-            observable -> {
-              try {
-                autofillLocation(editor.getText());
-              } catch (DBException e) {
-                e.printStackTrace();
-              }
-            });
-
+    cmbo_text.getEditor().setOnKeyTyped(this::locationTextChanged);
     LinkedList<String> languages = ServiceDB.getLanguages();
+    languages.add("French");
     ObservableList<String> langList = FXCollections.observableList(languages);
     cmbo_selectLang.setItems(langList);
   }
 
   @FXML
-  public void autofillLocation(String currentText) throws DBException {
+  public void autofillLocation(String currentText) {
     System.out.println(currentText);
-    if (currentText.length() > 1) {
-      fuzzySearchNodeList = FuzzySearchAlgorithm.suggestLocations(currentText);
+    if (currentText.length() > 2) {
+      try {
+        fuzzySearchNodeList = FuzzySearchAlgorithm.suggestLocations(currentText);
+      } catch (DBException e) {
+        e.printStackTrace();
+      }
       LinkedList<String> fuzzySearchStringList = new LinkedList<>();
       if (fuzzySearchNodeList != null) {
 
@@ -71,8 +64,60 @@ public class TemplateController implements Controller {
         fuzzySearchTextList = FXCollections.observableList(fuzzySearchStringList);
       }
       System.out.println(fuzzySearchTextList);
-      cmbo_text.getItems().setAll(fuzzySearchTextList);
     }
     if (fuzzySearchTextList == null) fuzzySearchTextList.add("  ");
+  }
+
+  // Handler for location combo box
+  @FXML
+  public void locationTextChanged(KeyEvent event) {
+    String curr = cmbo_text.getEditor().getText();
+    autofillLocation(curr);
+    cmbo_text.getItems().setAll(fuzzySearchTextList);
+    cmbo_text.show();
+  }
+
+  // Create Translator Request
+  @FXML
+  public void createNewTranslator() throws DBException {
+
+    String langSelection = cmbo_selectLang.getSelectionModel().getSelectedItem();
+    String nodeID;
+    int nodeIndex = 0;
+
+    try {
+      String curr = cmbo_text.getEditor().getText();
+      for (String name : fuzzySearchTextList) {
+        if (name.equals(curr)) {
+          nodeIndex++;
+          break;
+        }
+      }
+      nodeID = fuzzySearchNodeList.get(nodeIndex).getNodeID();
+      System.out.println(nodeID);
+    } catch (IndexOutOfBoundsException e) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText("Please select a location for your service request!");
+      errorAlert.show();
+      return;
+    }
+
+    String notes = txtf_langNotes.getText();
+    if (langSelection == null) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText("Please select a language for your translation request!");
+      errorAlert.show();
+      return;
+    }
+    int transReq = ServiceDB.addTransReq(notes, nodeID, langSelection);
+    App.adminDataStorage.addToList(transReq);
+
+    txtf_langNotes.clear();
+    cmbo_selectLang.getItems().clear();
+    cmbo_text.getItems().clear();
+
+    Alert confAlert = new Alert(Alert.AlertType.CONFIRMATION);
+    confAlert.setContentText("Request Recieved");
+    confAlert.show();
   }
 }
