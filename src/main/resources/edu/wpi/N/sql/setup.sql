439a05b84c82a27a06c863e20eb20a31955dd37e
seungho715 <krislee990715@gmail.com>
2020-04-28 22:53:04 -0400

    yess

--- src/main/resources/edu/wpi/N/sql/setup.sql
+++ src/main/resources/edu/wpi/N/sql/setup.sql
@@ -203,6 +203,10 @@ INSERT INTO service VALUES ('IT', '00:00', '00:00', 'Make a request for IT servi
 INSERT INTO service VALUES ('Internal Transportation', '00:00', '00:00', 'Make a request for Internal Transportation!');
 INSERT INTO service VALUES ('Security', '00:00', '00:00', 'Make a security request!');
 INSERT INTO service VALUES ('Flower', '00:00', '00:00', 'Make a request for Flower Delivery services');
+/** Hard coded the flowers, need to be deleted for iteration 3*/
+INSERT INTO flower VALUES ('Rose', 100);
+INSERT INTO flower VALUES ('Tulips', 100);
+INSERT INTO flower VALUES ('SAM', 1000);
 
 INSERT INTO credential VALUES ('Gaben', 'MoolyFTW', 'ADMIN');
 
