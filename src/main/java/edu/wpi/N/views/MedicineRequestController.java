d7409eca84da81b91abf33a71376c85e8669fe62
seungho715 <krislee990715@gmail.com>
2020-04-29 02:47:41 -0400

    please

--- src/main/java/edu/wpi/N/views/MedicineRequestController.java
+++ src/main/java/edu/wpi/N/views/MedicineRequestController.java
@@ -7,6 +7,7 @@ import edu.wpi.N.App;
 import edu.wpi.N.algorithms.FuzzySearchAlgorithm;
 import edu.wpi.N.database.*;
 import edu.wpi.N.entities.DbNode;
+import edu.wpi.N.entities.States.StateSingleton;
 import edu.wpi.N.entities.employees.Doctor;
 import edu.wpi.N.entities.request.MedicineRequest;
 import edu.wpi.N.entities.request.Request;
@@ -24,160 +25,163 @@ import javafx.scene.input.KeyEvent;
 
 public class MedicineRequestController implements Controller, Initializable {
 
-    private App mainApp;
+  private App mainApp;
 
-    ObservableList<Request> tableData = FXCollections.observableArrayList();
-    ObservableList<String> languageData = FXCollections.observableArrayList();
-    private LinkedList<DbNode> fuzzySearchPatientLocation = new LinkedList<>();
-    private ObservableList<String> fuzzySearchTextListPatientLocations =
-            FXCollections.observableArrayList();
-    private LinkedList<DbNode> fuzzySearchNodeList = new LinkedList<>();
-    LinkedList<String> longNamesList = new LinkedList<>(); // Stores Floor Node names
-    LinkedList<DbNode> allFloorNodes; // stores all the nodes on the floor
+  ObservableList<Request> tableData = FXCollections.observableArrayList();
+  ObservableList<String> languageData = FXCollections.observableArrayList();
+  private LinkedList<DbNode> fuzzySearchPatientLocation = new LinkedList<>();
+  private ObservableList<String> fuzzySearchTextListPatientLocations =
+      FXCollections.observableArrayList();
+  private LinkedList<DbNode> fuzzySearchNodeList = new LinkedList<>();
+  LinkedList<String> longNamesList = new LinkedList<>(); // Stores Floor Node names
+  LinkedList<DbNode> allFloorNodes; // stores all the nodes on the floor
 
-    @FXML ComboBox<Doctor> cb_doctors;
-    @FXML TableView tb_patients;
-    @FXML Label lbl_empname;
-    @FXML JFXTextField txtf_patient;
-    @FXML JFXTextField txtf_medicine;
-    @FXML JFXTextField txtf_dosage;
-    @FXML JFXTextArea txtf_notes;
-    @FXML JFXTextField txtf_patientLocation;
-    @FXML JFXListView lst_patientLocations;
+  @FXML ComboBox<Doctor> cb_doctors;
+  @FXML TableView tb_patients;
+  @FXML Label lbl_empname;
+  @FXML JFXTextField txtf_patient;
+  @FXML JFXTextField txtf_medicine;
+  @FXML JFXTextField txtf_dosage;
+  @FXML JFXTextArea txtf_notes;
+  @FXML JFXTextField txtf_patientLocation;
+  @FXML JFXListView lst_patientLocations;
 
-    @Override
-    public void setMainApp(App mainApp) {
-        this.mainApp = mainApp;
+  @Override
+  public void setMainApp(App mainApp) {
+    this.mainApp = mainApp;
+  }
+
+  @Override
+  public void setSingleton(StateSingleton singleton) {}
+
+  // Temp Placeholder for Testing
+  public void returnToRequests() throws IOException {
+    this.mainApp.switchScene("newHomePage.fxml", singleton);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    initializeTableOfPatients();
+
+    try {
+      populateDoctorList();
+      populateMedicineRequests();
+
+      allFloorNodes = MapDB.allNodes();
+      for (DbNode node : allFloorNodes) {
+        longNamesList.add(node.getLongName());
+      }
+
+    } catch (DBException e) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText(e.getMessage());
+      errorAlert.show();
     }
+  }
 
-    // Temp Placeholder for Testing
-    public void returnToRequests() throws IOException {
-        this.mainApp.switchScene("home.fxml");
+  public void populateDoctorList() throws DBException {
+    ObservableList<Doctor> docs = FXCollections.observableArrayList();
+    try {
+      docs.addAll(DoctorDB.getDoctors());
+      cb_doctors.getItems().addAll(docs);
+    } catch (DBException e) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText(e.getMessage());
+      errorAlert.show();
     }
+  }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        initializeTableOfPatients();
+  public void initializeTableOfPatients() {
+    TableColumn<MedicineRequest, String> patient = new TableColumn<>("Patient Name");
+    patient.setMaxWidth(150);
+    patient.setMinWidth(150);
+    patient.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("patient"));
 
-        try {
-            populateDoctorList();
-            populateMedicineRequests();
+    TableColumn<MedicineRequest, String> meds = new TableColumn<>("Medicine");
+    meds.setMaxWidth(150);
+    meds.setMinWidth(150);
+    meds.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("medicineName"));
 
-            allFloorNodes = MapDB.allNodes();
-            for (DbNode node : allFloorNodes) {
-                longNamesList.add(node.getLongName());
-            }
+    TableColumn<MedicineRequest, Double> dosage = new TableColumn<>("Dosage");
+    dosage.setMaxWidth(75);
+    dosage.setMinWidth(75);
+    dosage.setCellValueFactory(new PropertyValueFactory<MedicineRequest, Double>("dosage"));
 
-        } catch (DBException e) {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setContentText(e.getMessage());
-            errorAlert.show();
+    TableColumn<MedicineRequest, String> units = new TableColumn<>("Units");
+    units.setMaxWidth(75);
+    units.setMinWidth(75);
+    units.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("units"));
+
+    TableColumn<MedicineRequest, String> assignedDoctor = new TableColumn<>("Assigned Doctor");
+    assignedDoctor.setMaxWidth(150);
+    assignedDoctor.setMinWidth(150);
+    assignedDoctor.setCellValueFactory(
+        new PropertyValueFactory<MedicineRequest, String>("emp_assigned"));
+
+    TableColumn<MedicineRequest, String> notes = new TableColumn<>("Notes");
+    notes.setMaxWidth(150);
+    notes.setMinWidth(150);
+    notes.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("compNotes"));
+
+    tb_patients.getColumns().addAll(patient, meds, dosage, units, assignedDoctor, notes);
+  }
+
+  @FXML
+  public void createMedRequest() throws DBException {
+
+    int currentSelection = lst_patientLocations.getSelectionModel().getSelectedIndex();
+    DbNode medLocation = fuzzySearchNodeList.get(currentSelection);
+
+    try {
+
+      for (DbNode node : allFloorNodes) {
+        if (node.getNodeID().equals(medLocation.getNodeID())) {
+          ServiceDB.addMedReq(
+              txtf_notes.getText(),
+              medLocation.getNodeID(),
+              txtf_medicine.getText(),
+              Double.parseDouble(txtf_dosage.getText()),
+              "ml",
+              txtf_patient.getText());
         }
+      }
+    } catch (DBException e) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText(e.getMessage());
+      errorAlert.show();
     }
+  }
 
-    public void populateDoctorList() throws DBException {
-        ObservableList<Doctor> docs = FXCollections.observableArrayList();
-        try {
-            docs.addAll(DoctorDB.getDoctors());
-            cb_doctors.getItems().addAll(docs);
-        } catch (DBException e) {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setContentText(e.getMessage());
-            errorAlert.show();
-        }
-    }
+  public void fuzzySearchDoctorsOffices(KeyEvent keyInput) throws DBException {
+    String currentText = txtf_patientLocation.getText();
+    fuzzySearchPatientLocation = FuzzySearchAlgorithm.suggestLocations(currentText);
+    LinkedList<String> fuzzySearchStringList = new LinkedList<>();
+    if (fuzzySearchPatientLocation != null) {
 
-    public void initializeTableOfPatients() {
-        TableColumn<MedicineRequest, String> patient = new TableColumn<>("Patient Name");
-        patient.setMaxWidth(150);
-        patient.setMinWidth(150);
-        patient.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("patient"));
+      for (DbNode node : fuzzySearchPatientLocation) {
+        fuzzySearchStringList.add(node.getLongName());
+      }
 
-        TableColumn<MedicineRequest, String> meds = new TableColumn<>("Medicine");
-        meds.setMaxWidth(150);
-        meds.setMinWidth(150);
-        meds.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("medicineName"));
+      fuzzySearchTextListPatientLocations = FXCollections.observableList(fuzzySearchStringList);
+    } else fuzzySearchTextListPatientLocations = FXCollections.observableList(longNamesList);
+    lst_patientLocations.setItems(fuzzySearchTextListPatientLocations);
+  }
 
-        TableColumn<MedicineRequest, Double> dosage = new TableColumn<>("Dosage");
-        dosage.setMaxWidth(75);
-        dosage.setMinWidth(75);
-        dosage.setCellValueFactory(new PropertyValueFactory<MedicineRequest, Double>("dosage"));
+  @FXML
+  private void searchByLocationTextFill(KeyEvent inputMethodEvent) throws DBException {
+    String currentText = txtf_patientLocation.getText();
+    fuzzySearchNodeList = FuzzySearchAlgorithm.suggestLocations(currentText);
+    LinkedList<String> fuzzySearchStringList = new LinkedList<>();
+    if (fuzzySearchNodeList != null) {
 
-        TableColumn<MedicineRequest, String> units = new TableColumn<>("Units");
-        units.setMaxWidth(75);
-        units.setMinWidth(75);
-        units.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("units"));
+      for (DbNode node : fuzzySearchNodeList) {
+        fuzzySearchStringList.add(node.getLongName());
+      }
 
-        TableColumn<MedicineRequest, String> assignedDoctor = new TableColumn<>("Assigned Doctor");
-        assignedDoctor.setMaxWidth(150);
-        assignedDoctor.setMinWidth(150);
-        assignedDoctor.setCellValueFactory(
-                new PropertyValueFactory<MedicineRequest, String>("emp_assigned"));
-
-        TableColumn<MedicineRequest, String> notes = new TableColumn<>("Notes");
-        notes.setMaxWidth(150);
-        notes.setMinWidth(150);
-        notes.setCellValueFactory(new PropertyValueFactory<MedicineRequest, String>("compNotes"));
-
-        tb_patients.getColumns().addAll(patient, meds, dosage, units, assignedDoctor, notes);
-    }
-
-    @FXML
-    public void createMedRequest() throws DBException {
-
-        int currentSelection = lst_patientLocations.getSelectionModel().getSelectedIndex();
-        DbNode medLocation = fuzzySearchNodeList.get(currentSelection);
-
-        try {
-
-            for (DbNode node : allFloorNodes) {
-                if (node.getNodeID().equals(medLocation.getNodeID())) {
-                    ServiceDB.addMedReq(
-                            txtf_notes.getText(),
-                            medLocation.getNodeID(),
-                            txtf_medicine.getText(),
-                            Double.parseDouble(txtf_dosage.getText()),
-                            "ml",
-                            txtf_patient.getText());
-                }
-            }
-        } catch (DBException e) {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setContentText(e.getMessage());
-            errorAlert.show();
-        }
-    }
-
-    public void fuzzySearchDoctorsOffices(KeyEvent keyInput) throws DBException {
-        String currentText = txtf_patientLocation.getText();
-        fuzzySearchPatientLocation = FuzzySearchAlgorithm.suggestLocations(currentText);
-        LinkedList<String> fuzzySearchStringList = new LinkedList<>();
-        if (fuzzySearchPatientLocation != null) {
-
-            for (DbNode node : fuzzySearchPatientLocation) {
-                fuzzySearchStringList.add(node.getLongName());
-            }
-
-            fuzzySearchTextListPatientLocations = FXCollections.observableList(fuzzySearchStringList);
-        } else fuzzySearchTextListPatientLocations = FXCollections.observableList(longNamesList);
-        lst_patientLocations.setItems(fuzzySearchTextListPatientLocations);
-    }
-
-    @FXML
-    private void searchByLocationTextFill(KeyEvent inputMethodEvent) throws DBException {
-        String currentText = txtf_patientLocation.getText();
-        fuzzySearchNodeList = FuzzySearchAlgorithm.suggestLocations(currentText);
-        LinkedList<String> fuzzySearchStringList = new LinkedList<>();
-        if (fuzzySearchNodeList != null) {
-
-            for (DbNode node : fuzzySearchNodeList) {
-                fuzzySearchStringList.add(node.getLongName());
-            }
-
-            fuzzySearchTextListPatientLocations = FXCollections.observableList(fuzzySearchStringList);
-        } else fuzzySearchTextListPatientLocations = FXCollections.observableList(longNamesList);
-        lst_patientLocations.setItems(fuzzySearchTextListPatientLocations);
-    }
+      fuzzySearchTextListPatientLocations = FXCollections.observableList(fuzzySearchStringList);
+    } else fuzzySearchTextListPatientLocations = FXCollections.observableList(longNamesList);
+    lst_patientLocations.setItems(fuzzySearchTextListPatientLocations);
+  }
 
   /*
   public void populateTable() throws DBException {
@@ -205,24 +209,24 @@ public class MedicineRequestController implements Controller, Initializable {
   }
    */
 
-    public void populateMedicineRequests() throws DBException {
+  public void populateMedicineRequests() throws DBException {
 
-        try {
-            LinkedList<Request> medReq = new LinkedList<>();
-            for (Request allReq : ServiceDB.getRequests()) {
-                if (allReq.getServiceType().equals("MedicineRequest")) ;
-                medReq.add(allReq);
-            }
+    try {
+      LinkedList<Request> medReq = new LinkedList<>();
+      for (Request allReq : ServiceDB.getRequests()) {
+        if (allReq.getServiceType().equals("MedicineRequest")) ;
+        medReq.add(allReq);
+      }
 
-            tb_patients.getItems().clear();
-            ObservableList<Request> allMedReq = FXCollections.observableArrayList();
-            allMedReq.addAll(medReq);
-            tb_patients.setItems(allMedReq);
+      tb_patients.getItems().clear();
+      ObservableList<Request> allMedReq = FXCollections.observableArrayList();
+      allMedReq.addAll(medReq);
+      tb_patients.setItems(allMedReq);
 
-        } catch (DBException e) {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-            errorAlert.setContentText(e.getMessage());
-            errorAlert.show();
-        }
+    } catch (DBException e) {
+      Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+      errorAlert.setContentText(e.getMessage());
+      errorAlert.show();
     }
+  }
 }
