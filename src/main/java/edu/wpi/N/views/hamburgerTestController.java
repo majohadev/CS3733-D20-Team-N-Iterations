8f9d0b3dcb28db89f18fef9edef49da6afa95d87
Nicholas Wood <woodn123@gmail.com>
2020-04-29 05:34:49 -0400

    fixes algo

--- src/main/java/edu/wpi/N/views/hamburgerTestController.java
+++ src/main/java/edu/wpi/N/views/hamburgerTestController.java
@@ -2,7 +2,6 @@ package edu.wpi.N.views;
 
 import com.jfoenix.controls.*;
 import edu.wpi.N.App;
-import edu.wpi.N.algorithms.Algorithm;
 import edu.wpi.N.algorithms.FuzzySearchAlgorithm;
 import edu.wpi.N.database.DBException;
 import edu.wpi.N.database.MapDB;
@@ -254,9 +253,8 @@ public class hamburgerTestController implements Controller, Initializable {
     }
     if (node1.getFloor() <= node2.getFloor()) {
       Path path;
-      Algorithm myAStar = new Algorithm();
       try {
-        path = myAStar.findPath(node1, node2, handicap);
+        path = singleton.savedAlgo.findPath(node1, node2, handicap);
         ArrayList<String> directions = path.getDirections();
         for (String s : directions) {
           System.out.println(s);
