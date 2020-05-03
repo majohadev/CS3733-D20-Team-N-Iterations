cda536c1f1a02d02aa61d5ece01e836afdd4b282
TianYuFan1 <tianyufan01@gmail.com>
2020-05-03 19:51:50 -0400

    will add the functionalities now

--- src/main/java/edu/wpi/N/views/mapDisplay/NewMapDisplayController.java
+++ src/main/java/edu/wpi/N/views/mapDisplay/NewMapDisplayController.java
@@ -3,10 +3,16 @@ package edu.wpi.N.views.mapDisplay;
 import edu.wpi.N.App;
 import edu.wpi.N.entities.States.StateSingleton;
 import edu.wpi.N.views.Controller;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 
 public class NewMapDisplayController implements Controller {
   private App mainApp;
   private StateSingleton singleton;
+
+  @FXML Pane pn_iconBar;
+  @FXML Pane pn_change;
 
   public void initialize() {}
 
@@ -15,5 +21,8 @@ public class NewMapDisplayController implements Controller {
     this.mainApp = mainApp;
   }
 
-
+  public void onLocationIconClicked(MouseEvent e) {
+    pn_iconBar.getChildren().forEach(n -> n.setStyle("-fx-background-color: #263051"));
+    ((Pane) e.getSource()).setStyle("-fx-background-color: #4A69C6;");
+  }
 }
