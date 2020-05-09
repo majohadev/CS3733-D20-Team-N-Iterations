061112d3c853fab334af3f376f71b22be513619d
Evdude33 <evanllewellyn33@gmail.com>
2020-05-08 21:24:54 -0400

    progress so far

--- src/main/java/edu/wpi/N/Main.java
+++ src/main/java/edu/wpi/N/Main.java
@@ -23,24 +23,6 @@ public class Main {
     CSVParser.parseCSV(INPUT_NODES_DEFAULT);
     CSVParser.parseCSV(INPUT_EDGES_DEFAULT);*/
 
-    Originator originator = new Originator();
-    CareTaker careTaker = new CareTaker();
-
-    originator.setState("State #1");
-    originator.setState("State #2");
-    careTaker.add(originator.saveStateToMemento());
-
-    originator.setState("State #3");
-    careTaker.add(originator.saveStateToMemento());
-
-    originator.setState("State #4");
-    System.out.println("Current State: " + originator.getState());
-
-    originator.getStateFromMemento(careTaker.get(0));
-    System.out.println("First saved State: " + originator.getState());
-    originator.getStateFromMemento(careTaker.get(1));
-    System.out.println("Second saved State: " + originator.getState());
-
     App.launch(App.class, args);
   }
 }
