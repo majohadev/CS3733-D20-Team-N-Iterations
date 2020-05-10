4e4765c748870c344e35533162a6fd18190cb60b
Ivan <ieroshenko@wpi.edu>
2020-05-09 22:54:34 -0400

    Merged dev into my branch

--- src/main/java/edu/wpi/N/App.java
+++ src/main/java/edu/wpi/N/App.java
@@ -3,10 +3,8 @@ package edu.wpi.N;
 import edu.wpi.N.database.DBException;
 import edu.wpi.N.entities.States.StateSingleton;
 import edu.wpi.N.views.Controller;
-
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,65 +14,65 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class App extends Application {
-    private Stage masterStage;
+  private Stage masterStage;
 
-    @Override
-    public void init() {
-        log.info("Starting Up");
-    }
+  @Override
+  public void init() {
+    log.info("Starting Up");
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws IOException, DBException {
-        // Configure the primary Stage
+  @Override
+  public void start(Stage primaryStage) throws IOException, DBException {
+    // Configure the primary Stage
 
-        this.masterStage = primaryStage;
-        this.masterStage.setTitle("Brigham and Women's Hospital Kiosk Application");
+    this.masterStage = primaryStage;
+    this.masterStage.setTitle("Brigham and Women's Hospital Kiosk Application");
 
-        StateSingleton newSingleton = StateSingleton.getInstance();
-        switchScene("views/mapDisplay/newMapDisplay.fxml", newSingleton);
-//    switchScene("views/chatbot/chatBox.fxml", newSingleton);
-        masterStage.setMaximized(true);
-    }
+    StateSingleton newSingleton = StateSingleton.getInstance();
+    switchScene("views/mapDisplay/newMapDisplay.fxml", newSingleton);
+    //    switchScene("views/chatbot/chatBox.fxml", newSingleton);
+    masterStage.setMaximized(true);
+  }
 
-    public Stage getStage() {
-        return this.masterStage;
-    }
+  public Stage getStage() {
+    return this.masterStage;
+  }
 
-    @Override
-    public void stop() {
-        log.info("Shutting Down");
-    }
+  @Override
+  public void stop() {
+    log.info("Shutting Down");
+  }
 
-    public void switchScene(String path, StateSingleton singleton) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource(path));
+  public void switchScene(String path, StateSingleton singleton) throws IOException {
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource(path));
 
-        // Inject Singleton object into classes with Constructors that take StateSingleton
-        loader.setControllerFactory(
-                type -> {
-                    try {
-                        // look for constructor taking StateSingleton as a parameter
-                        for (Constructor<?> c : type.getConstructors()) {
-                            if (c.getParameterCount() == 1) {
-                                if (c.getParameterTypes()[0] == StateSingleton.class) {
-                                    return c.newInstance(singleton);
-                                }
-                            }
-                        }
-                        // didn't find appropriate constructor, just use default constructor:
-                        return type.getConstructor().newInstance();
-                    } catch (Exception exc) {
-                        throw new RuntimeException(exc);
-                    }
-                });
+    // Inject Singleton object into classes with Constructors that take StateSingleton
+    loader.setControllerFactory(
+        type -> {
+          try {
+            // look for constructor taking StateSingleton as a parameter
+            for (Constructor<?> c : type.getConstructors()) {
+              if (c.getParameterCount() == 1) {
+                if (c.getParameterTypes()[0] == StateSingleton.class) {
+                  return c.newInstance(singleton);
+                }
+              }
+            }
+            // didn't find appropriate constructor, just use default constructor:
+            return type.getConstructor().newInstance();
+          } catch (Exception exc) {
+            throw new RuntimeException(exc);
+          }
+        });
 
-        Pane pane = loader.load();
-        Controller controller = loader.getController();
-        controller.setMainApp(this);
-        Scene scene = new Scene(pane);
-        masterStage.setScene(scene);
-        // masterStage.setMaximized(true);
-        masterStage.setFullScreenExitHint("");
-        masterStage.show();
-    }
+    Pane pane = loader.load();
+    Controller controller = loader.getController();
+    controller.setMainApp(this);
+    Scene scene = new Scene(pane);
+    masterStage.setScene(scene);
+    // masterStage.setMaximized(true);
+    masterStage.setFullScreenExitHint("");
+    masterStage.show();
+  }
 }
