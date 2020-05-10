a4b43b5f63ab9ec0b242e014fad9681c7e1de19a
kofichtner <54476324+kofichtner@users.noreply.github.com>
2020-05-09 21:46:39 -0400

    fixes merge conflict

--- src/main/java/edu/wpi/N/views/mapDisplay/NewMapDisplayController.java
+++ src/main/java/edu/wpi/N/views/mapDisplay/NewMapDisplayController.java
@@ -811,7 +811,9 @@ public class NewMapDisplayController extends QRGenerator implements Controller {
         pathMain.size() > 0
             ? pathMain.getDirections(singleton.savedAlgo.getMapData())
             : new ArrayList<>();
-        pathMain.size() > 0 ? pathMain.getDirections() : new ArrayList<>();
+    if (pathMain.size() > 0) {
+      pathMain.getDirections(singleton.savedAlgo.getMapData());
+    }
     ArrayList<Direction> googleDirections = new ArrayList<>();
 
     mapQRController.setTabs(
